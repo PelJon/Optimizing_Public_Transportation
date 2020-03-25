4db01fa2d742616826522ee0cd8b38a2c2129e8f
PelJon <Pelle.Svante.John@gmail.com>
2020-03-25 07:55:34 +0100

    Update turnstile.py

--- producers/models/turnstile.py
+++ producers/models/turnstile.py
@@ -34,13 +34,12 @@ class Turnstile(Producer):
         # Complete the below by deciding on a topic name, number of partitions, and number of replicas
         #
         #
-        topic_name = f"com.udacity.chicagotransit.station_turnstiles"
         super().__init__(
-            topic_name, # Come up with a better topic name
+            "org.chicago.cta.turnstiles", # Come up with a better topic name
             key_schema=Turnstile.key_schema,
             value_schema=Turnstile.value_schema,
-            num_partitions=4,
-            num_replicas=2,
+            num_partitions=1,
+            num_replicas=1
         )
         self.station = station
         self.turnstile_hardware = TurnstileHardware(station)
@@ -48,18 +47,20 @@ class Turnstile(Producer):
     def run(self, timestamp, time_step):
         """Simulates riders entering through the turnstile."""
         num_entries = self.turnstile_hardware.get_entries(timestamp, time_step)
-        logger.info("turnstile kafka integration incomplete - skipping")
+        #logger.info("turnstile kafka integration incomplete - skipping")
         #
         #
         # Complete this function by emitting a message to the turnstile topic for the number
         # of entries that were calculated
         #
-        self.producer.produce(
-            topic=self.topic_name,
-            key={"timestamp": self.time_millies()},
-            value={
-                "station_id": self.station.sation_id,
-                "station_name":self.station.name,
-                "line": self.station.color,
+        for _ in range (num_entries):
+            self.producer.produce(
+                topic=self.topic_name,
+                key={"timestamp": self.time_millis()},
+                value={
+                    "station_id": self.station.station_id,
+                    "station_name":self.station.name,
+                    "line": self.station.color.name,
             },
         )
+        logger.info("Turnstile message successfully run")
